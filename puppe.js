6dd5350c1ebf1069d07ba1d0ca43751a5041cb62
DeividXupon <deivid.silva.info@gmail.com>
2025-05-17 22:19:35 -0300

    add logic login

--- puppe.js
+++ puppe.js
@@ -1,14 +1,35 @@
 const puppeteer = require('puppeteer');
 const WebSocket = require('ws');
+const { exec } = require('child_process');
 
 const PORT = 8080;
 const wsServer = new WebSocket.Server({ port: PORT });
 
+function userNotLoggedIn() {
+    console.log("Nova janela de login do Google detectada!");
+
+    setTimeout(async () => {
+        exec('xdotool search --name "login" windowactivate key Tab Tab Return', (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Erro ao executar xdotool: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.error(`Erro do xdotool: ${stderr}`);
+                return;
+            }
+            console.log(`Saída do xdotool: ${stdout}`);
+        });
+    }, 2000);
+}
+
 wsServer.on('connection', (ws) => {
     console.log('Conectado ao WebSocket.');
     ws.on('message', async (message) => {
+        const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
         try {
             const browser = await puppeteer.launch({
+                headless: false,
                 userDataDir: '/home/deivid/.config/google-chrome',
                 args: [
                     '--start-maximized',
@@ -32,6 +53,71 @@ wsServer.on('connection', (ws) => {
             });
 
             await page.goto('https://www.binance.com/pt-BR/my/dashboard');
+
+            if (page.url().includes('login')) {
+                await page.hover('#googleLoginBtn');
+                console.log("Mouse movido para o botão '#googleLoginBtn'!");
+                await page.click('#googleLoginBtn')
+                console.log("Nova janela de login do Google detectada!");
+
+                await new Promise((resolve) => {
+                    setTimeout(() => {
+                        exec('xdotool search --name "login" windowactivate key Tab Tab Return', (error, stdout, stderr) => {
+                            if (error) {
+                                console.error(`Erro ao executar xdotool: ${error.message}`);
+                                resolve();
+                                return;
+                            }
+                            if (stderr) {
+                                console.error(`Erro do xdotool: ${stderr}`);
+                                resolve();
+                                return;
+                            }
+                            console.log(`Saída do xdotool: ${stdout}`);
+                            resolve();
+                        });
+                    }, 3000);
+                });
+
+                await delay(6000);
+
+                const checkbox = await page.$('div[role="checkbox"]');
+                if (checkbox) {
+                    await checkbox.click();
+                    console.log("Checkbox clicado!");
+                } else {
+                    console.log("Checkbox não encontrado!");
+                }
+
+                const button = await page.$('button.bn-button.bn-button__primary');
+                if (button) {
+                    await button.click();
+                    console.log("Botão clicado!");
+                } else {
+                    console.log("Botão não encontrado!");
+                }
+                
+                await delay(5000);
+
+                if (page.url().includes('login')) {
+                    console.log("Página de login ainda aberta!");
+                    ws.send(JSON.stringify({
+                        success: false,
+                        message: 'Login pelo google travou muitas tentativas.'
+                    }));
+                    ws.close();
+                    browser.close();
+                }
+
+                ws.send(JSON.stringify({
+                    success: true,
+                    reTry: true,
+                    message: 'Login realizado com sucesso!'
+                }));
+                ws.close();
+                browser.close();
+            }
+
             await page.waitForSelector('button.deposit-btn');
             await page.click('button.deposit-btn');
             console.log("Botão 'Depositar' clicado!");
@@ -41,6 +127,11 @@ wsServer.on('connection', (ws) => {
             console.log("Elemento 'BRL' clicado!");
 
             await page.waitForSelector('img[alt="Transferência Bancária (PIX)"]');
+            await delay(1000);
+            await page.click('img[alt="Transferência Bancária (PIX)"]');
+            await delay(1000);
+            await page.click('img[alt="Transferência Bancária (PIX)"]');
+            await delay(1000);
             await page.click('img[alt="Transferência Bancária (PIX)"]');
             console.log("Elemento 'pix' clicado!");
 
@@ -73,10 +164,14 @@ wsServer.on('connection', (ws) => {
             //console.log("Resposta da API:", response.data);
 
             console.log("Texto copiado!" + textPix);
-            ws.send(textPix);
+            ws.send(JSON.stringify({
+                success: true,
+                data: textPix,
+            }));
 
             browser.close();
             ws.close();
+
             console.log("Conexão WebSocket fechada.");
         } catch (error) {
             console.error('Erro ao processar a página:', error);
@@ -86,4 +181,3 @@ wsServer.on('connection', (ws) => {
 });
 
 console.log(`Servidor WebSocket rodando na porta ${PORT}`);
-
